b800f94754aba35d5d40797d397b06f11d5ad8bf
wanyihua <wanyihua1028@163.com>
2016-11-28 00:05:01 +0800

    add home directory

---
+++ application/home/controller/Useraddress.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Create By: PhpStorm
+ * User: yihua
+ * File: Useraddress.php
+ * Date: 2016/11/27
+ * Time: 23:48
+ */
+
+namespace app\home\controller;
+
+use think\Controller;
+use think\Log;
+use app\home\model\UserAddress as UserAddressModel;
+
+class UserAddress extends Controller
+{
+    public function addUserAddress($param)
+    {
+        $address = new UserAddressModel();
+        $address->address_name = $param['user_id'];
+        $address->address_name = $param['address_name'];
+        $address->address_name = $param['consignee'];
+        $address->address_name = $param['country'];
+        $address->address_name = $param['province'];
+        $address->address_name = $param['city'];
+        $address->address_name = $param['district'];
+        $address->address_name = $param['address'];
+        $address->address_name = $param['zipcode'];
+        $address->address_name = $param['zipcode'];
+        $address->address_name = $param['tel'];
+        $address->address_name = $param['mobile'];
+        if($address->save())
+        {
+            Log::info("Insert UserAddress OK");
+            return true;
+        }
+        else
+        {
+            Log::info("Insert UserAddress Failed");
+            return false;
+        }
+    }
+}
+
---
+++ application/home/model/UserAddress.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Create By: PhpStorm
+ * User: yihua
+ * File: UserAddress.php
+ * Date: 2016/11/27
+ * Time: 23:13
+ */
+namespace app\home\model;
+use think\Model;
+
+class UserAddress extends Model
+{
+    //set talbename
+    protected $name = "user_address";
+}
