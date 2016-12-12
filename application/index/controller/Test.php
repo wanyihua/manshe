85e1ccb8acecdddee4395deaaeea14e236dc0060
jsbdjvk <469523020@qq.com>
2016-12-12 23:35:39 +0800

    test

--- application/index/controller/Test.php
+++ application/index/controller/Test.php
@@ -1,10 +1,16 @@
 <?php
 namespace app\index\controller;
 
+use think\cache\driver\Redis;
+
 class Test
 {
     public function test()
     {
+        $objRedis = new Redis();
+        $objRedis->set('aaaaa', 'acacac');
+        $ret = $objRedis->get('aaaaa');
+        var_dump($ret);
         return array('a'=>1, 'b'=>'c');
     }
 }
