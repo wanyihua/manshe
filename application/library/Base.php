f02f6829d8f1ea7b1a182217fda0c739464adb31
yuanxuncheng <405425950@qq.com>
2016-12-18 00:48:56 +0800

    修改白名单配置

--- application/library/Base.php
+++ application/library/Base.php
@@ -23,7 +23,7 @@ class Base extends Controller
     public function __construct()
     {
         parent::__construct();
-        $this->redis = RedisMgr::getInstance();
+        //$this->redis = RedisMgr::getInstance();
         
         $this->errno = 0;
         $this->data = array();
@@ -45,7 +45,7 @@ class Base extends Controller
     public function checkAuth() {
         //$this->redis->set('session', 'test');
         //$sesion = $this->redis->get('session');
-        Log::log("chekauth".var_export($this->redis));
+        Log::log("chekauth");
     }
 
     /**
