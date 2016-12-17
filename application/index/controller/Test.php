3c17b7f13b66997883424a38858b2b42289877b1
jsbdjvk <469523020@qq.com>
2016-12-17 15:56:12 +0800

    模板测试

--- application/index/controller/Test.php
+++ application/index/controller/Test.php
@@ -1,19 +1,14 @@
 <?php
 namespace app\index\controller;
 
-use think\cache\driver\Redis;
 use think\Db;
+use think\View;
 
 class Test
 {
     public function test()
     {
-        $ret = Db::table('test')->where('user_name',1)->select();
-        Db::query('select * from think_user where id=?',[8]);
-        $objRedis = new Redis();
-        $objRedis->set('aaaaa', 'acacac');
-        $ret = $objRedis->get('aaaaa');
-        var_dump($ret);
-        return array('a'=>1, 'b'=>'c');
+        $view = new View();
+        $view->assign('aaa', 'hello world!');
     }
 }
