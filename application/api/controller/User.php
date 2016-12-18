cdbc46f1ce89ca5d3d52e8794937cd5ad2137357
yuanxuncheng <405425950@qq.com>
2016-12-18 23:40:42 +0800

    修改白名单配置

--- application/api/controller/User.php
+++ application/api/controller/User.php
@@ -9,18 +9,27 @@
 namespace app\api\controller;
 
 use app\library\Base as BaseController;
+use think\Request;
+use app\library\Error;
 
 class User extends BaseController {
+    private $param;
 
     public function __construct() {
-        
+        $this->param = Request::instance()->param();
     }
 
     public function login() {
+        if (!isset($this->param['identity_type'])
+            || !isset($this->param['identifier'])
+            || !isset($this->param['credential'])) {
+            return $this->getRes(Error::ERR_PARAM);
+        }
 
+        return $this->getRes();
     }
 
     public function logout() {
-
+        return $this->getRes();
     }
 }
