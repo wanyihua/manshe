e6f915f9dcff43b53675ff192f2070a0489830ec
wanyihua <wanyihua1028@163.com>
2016-12-26 02:34:27 +0800

    把home下的useraddress迁移到api模块下，同步更新route信息

--- application/route.php
+++ application/route.php
@@ -24,10 +24,10 @@ return [
     'user/login' => 'api/User/login',
     'user/logout' => 'api/User/logout',
     //个人中心
-    'address/adduseraddress' => ['home/UserAddress/addUserAddress',['method' => 'get']],//增加用户地址
-    'address/getuseraddress' => ['home/UserAddress/getUserAddress',['method' => 'get']],//查询用户有效地址
-    'address/removeuseraddress' => ['home/UserAddress/removeUseraddress',['method' => 'get']],//删除用户地址
-    'address/updateuseraddress' => ['home/UserAddress/updateUseraddress',['method' => 'get']],//删除用户地址
+    'address/adduseraddress' => ['api/UserAddress/addUserAddress',['method' => 'get']],//增加用户地址
+    'address/getuseraddress' => ['api/UserAddress/getUserAddress',['method' => 'get']],//查询用户有效地址
+    'address/removeuseraddress' => ['api/UserAddress/removeUseraddress',['method' => 'get']],//删除用户地址
+   'address/updateuseraddress' => ['api/UserAddress/updateUseraddress',['method' => 'get']],//删除用户地址
     'userfavorite/add' => ['api/UserFavorite/addFavorite',['method' => 'get']],//增加收藏帖子
     'userfavorite/remove' => ['api/UserFavorite/removeFavorite',['method' => 'get']],//删除收藏帖子
     'userfavorite/get' => ['api/UserFavorite/getFavorite',['method' => 'get']],//获取收藏帖子列表
