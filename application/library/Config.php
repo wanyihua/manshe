52f97ef3430f87eb2a518c9fa3f93b5e825710cc
yuanxuncheng <yuanxuncheng@iwaimai.baidu.com>
2016-12-27 23:48:23 +0800

    修改公共方法

---
+++ application/library/Config.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016-12-27
+ * Time: 23:17
+ */
+
+namespace app\library;
+
+class Config {
+
+    // 短信
+    const SMS_URL = 'https://sms.aliyuncs.com/?';
+    const SMS_KEY_ID = 'LTAIOFNv24vMvUNZ';
+    const SMS_KEY_SECRET = 'I98e0hT7o8vRvL4cDkxdeI5tFBnIdp';
+    
+
+}
